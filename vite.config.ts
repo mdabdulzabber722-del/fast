6a9a6e9398235b3d3bd4fb4da4e4f78f20c5ee9a
mdabdulzabber722-del <mdabdulzabber722@gmail.com>
2025-08-27 19:10:46 +0600

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: '.',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-    sourcemap: false,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html')
-      }
-    }
+    sourcemap: false
   },
   server: {
     port: 3000,
@@ -23,10 +15,5 @@ export default defineConfig({
   preview: {
     port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
     host: '0.0.0.0'
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src')
-    }
   }
 })
